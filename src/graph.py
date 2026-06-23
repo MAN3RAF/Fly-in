87502cdc0e14d6d4206d7f0ae6407a563046b8c2
MAN3RAF <lhsnsbar7@gmail.com>
2026-06-23 12:13:26 +0100

    up

--- src/graph.py
+++ src/graph.py
@@ -1,5 +1,6 @@
 from typing import List, Dict, Any
 from zone import Zone
+from drone import Drone
 
 
 class Graph():
@@ -11,8 +12,10 @@ class Graph():
 		self.hubs = hubs
 		self.connections = connections
 		self.zones: List[Zone] = []
+		self.connects: Dict[Zone,Zone] = {}
+		self.drones: List[Drone] = []
 
-	def get_hubs(self):
+	def get_hubs(self) -> List[Zone]:
 
 		for hub in self.hubs:
 			
@@ -34,3 +37,35 @@ class Graph():
 			self.zones.append(zone)
 
 		return self.zones
+
+	def get_connections(self) -> Dict[Zone, Zone]:
+
+		for conn in self.connections:
+			if not self.zones:
+				self.zones = self.get_hubs()
+			for zone in self.zones:
+				if zone.name in conn: #if name of zone in connection in our zones.
+					link_1: Zone = zone
+					break
+			for zone in self.zones:
+				if zone.name in conn.values(): #if name of zone in connection in our zones.
+					link_2: Zone = zone
+					break
+			self.connects[link_1] = link_2
+
+		return self.connects
+	
+	def get_drones(self):
+
+		for zone in self.zones:
+			if "start" in zone.name:
+				coords: tuple[int] = zone.coords
+		for zone in self.zones:
+			if "goal" in zone.name:
+				end_zone = zone
+
+		for i in range(1, self.nb_drones):
+			drone = Drone(i, coords, [], end_zone)
+			self.drones.append(drone)
+		return 
+
--- src/main.py
+++ src/main.py
@@ -10,20 +10,32 @@ parser = Parser()
 
 graph = parser.parse_map("maps/easy/01_linear_path.txt")
 
-print(graph.get_hubs())
+# print(graph.get_hubs()[1].name)
+# print(graph.get_connections())
 
 
-# Initialize all imported pygame modules
-pygame.init()
 
-# Create a display window
-screen = pygame.display.set_mode((800, 600))
 
-# Game loop
-running = True
-while running:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
 
-pygame.quit()
+
+
+
+
+
+
+
+
+# # Initialize all imported pygame modules
+# pygame.init()
+
+# # Create a display window
+# screen = pygame.display.set_mode((800, 600))
+
+# # Game loop
+# running = True
+# while running:
+#     for event in pygame.event.get():
+#         if event.type == pygame.QUIT:
+#             running = False
+
+# pygame.quit()
